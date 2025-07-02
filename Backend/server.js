efb3479fccce3348d1ee42bcc42236e5456621a8
RAJASEKAR-01 <hansomekillerraja@gmail.com>
2025-07-02 18:14:53 +0530

    DataBase

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 
 console.log('Mongo URI:', process.env.MONGO_URL);
 
@@ -19,7 +20,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 }).then(() => {
   console.log("MongoDB Connected")
-  app.listen(process.env.PORT, () => {
+  app.listen(PORT, () => {
     console.log('Server running on port', process.env.PORT);
   });
 }).catch((err) => console.error(err) 
